770c5da080b833f68e8747c936e114218d74889f
Hectornt <hemaocca@gmail.com>
2021-09-16 15:31:46 -0500

    MOD2

--- ProyectosJava/Proyecto1/src/App.java
+++ ProyectosJava/Proyecto1/src/App.java
@@ -1,5 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        int intNumero1 = 1;
+        int intNumero2 = 2;
+        int intSuma = 0;
+        int intCantidadGenerada = 10;
+        int i = 0;
+        while (i < intCantidadGenerada) {
+            intSuma = intNumero1 + intNumero2;
+            intNumero1 = intNumero2;
+            intNumero2 = intNumero2 + 1;
+            i++;
+            System.out.println(i + "._" + intSuma);
+
+        }
+
     }
 }
